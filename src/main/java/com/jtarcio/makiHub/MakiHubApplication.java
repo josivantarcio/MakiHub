4a936e9747dadda28de6e4742e50b0b7a897d291
Josivan Tarcio <josivantarcio@gmail.com>
2025-11-13 13:10:28 -0300

    Initial commit: MakiHub API project setup

---
+++ src/main/java/com/jtarcio/makiHub/MakiHubApplication.java
@@ -0,0 +1,13 @@
+package com.jtarcio.makiHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakiHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakiHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtarcio/makiHub/MakiHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtarcio.makiHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakiHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
